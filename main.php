4c74fa820f11f0214accb6a727dcafe40e335861
roshankc123 <kcroshan566@gmail.com>
2020-05-02 12:39:51 +0545

    fixes

--- main.php
+++ main.php
@@ -1,7 +1,5 @@
 <?php
 error_reporting(0);
-$conn=mysqli_connect("127.0.0.1","root","","tree");
-if(!$conn){ die(mysqli_connect_error());}
 header('Access-Control-Allow-Origin: *');
     if($_FILES['u_image']['tmp_name']){
       $image="images/".$_REQUEST['div_id'].".png";
@@ -14,6 +12,8 @@ header('Access-Control-Allow-Origin: *');
         //echo $image."::done";
       }
       if($_GET['user']){
+        $conn=mysqli_connect("127.0.0.1","root","","tree");
+        if(!$conn){ die(mysqli_connect_error());}
         $user=str_replace(array("'","-"),array("&qot","&das"),$_GET['user']);
       }
       if($_POST['json_file']){
@@ -25,7 +25,7 @@ header('Access-Control-Allow-Origin: *');
         if(!$qry){ echo mysqli_error($conn); }
       }
       if($_GET['get_json']==1){
-        $qry=mysqli_query($conn,"select u_json from data where u_cookie='".$user."' order by sn desc limit 1");
+        $qry=mysqli_query($conn,"select u_json from data where def=1 and u_cookie='".$user."' order by sn desc limit 1");
         if(!$qry){echo mysqli_error($conn);}
         $data=mysqli_fetch_all($qry);
         $json_file_filter=str_replace(array("&qot","&das"),array("'","-"),$data[0][0]);
